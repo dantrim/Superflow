34c34bf232c2d5ff02cc69cac868abb034d05085
Daniel Joseph Antrim <dantrim@uclhc-1.ps.uci.edu>
2015-10-05 06:58:32 -0700

    Update condor script

--- run/RunCondorSFlow.py
+++ run/RunCondorSFlow.py
@@ -7,6 +7,7 @@ import glob
 
 
 ana_name = "wwlikeAna" # name of executable (i.e. your executable running Superflow)
+start_dir = "/home/dantrim/n0216val/"  
 log_dir = "/home/dantrim/test_condor/histoAna/data/logs/" # output directory for your job and condor logs
 out_dir = "/home/dantrim/test_condor/histoAna/data/Raw/" # output directory for your output ntuples
 
@@ -15,34 +16,28 @@ samples = ["condor_lists"] # name of filelist inside of 'filelist_dir' for sampl
 
 def main() :
     print "RunCondorSFlow"
-
     print "Looking for sample filelists in main filelist directory : %s"%filelist_dir
 
     for s in samples :
         print "Submitting sample : %s"%s
-
         suffix_ = ""
         if not s.endswith("/") : suffix_+= "/"
         sample_lists = glob.glob(filelist_dir + s + suffix_ + "*.txt")
-
         for dataset in sample_lists :
+            print "    > %s"%dataset
             name  = dataset.split("/")
             name  = name[-1]
             name  = name.replace(".txt", "")
-
             condor_name = name + ".condor"
-
             condor_sub_file = open(condor_name, "w")
-            get_sub_script(ana_name, condor_sub_file, name, dataset, out_dir)
+            get_sub_script(ana_name, condor_sub_file, name, dataset, start_dir, out_dir)
             condor_sub_file.close()
-
             cmd = "condor_submit %s"%condor_name
             subprocess.call(cmd, shell=True)
-
             cmd = "rm %s"%condor_name
             subprocess.call(cmd, shell=True)
 
-def get_sub_script(ana = "", file_ = None, name = "", dataset = "", outputdir = "") :
+def get_sub_script(ana = "", file_ = None, name = "", dataset = "", startdir = "", outputdir = "") :
     """
     Write a condor submission script with the output logs, etc...
     associated with the currently submitted dataset.
@@ -52,6 +47,7 @@ def get_sub_script(ana = "", file_ = None, name = "", dataset = "", outputdir = 
     'name'      = dataset descriptive name to be used in the log names 
     'dataset'   = input .txt file to be provided to your executable 'ana' that
                   contains the FAX container names for this sample/dataset
+    'startdir'  = main working directory (RootCore dir) for Superflow
     'outputdir' = directory where the output ntuples will end up
 
     See [1] for documentation on setting up the condor submission scripts.
@@ -62,7 +58,6 @@ def get_sub_script(ana = "", file_ = None, name = "", dataset = "", outputdir = 
     if ana == "" :
         print "get_sub_script ERROR   input analysis name (i.e. executable) is empty."
         sys.exit()
-
     if not file_ :
         print "get_sub_script ERROR    input file is None."
         sys.exit()
@@ -71,6 +66,9 @@ def get_sub_script(ana = "", file_ = None, name = "", dataset = "", outputdir = 
         sys.exit()
     if dataset == "" :
         print "get_sub_script ERROR    dataset is empty."
+        sys.exit()
+    if startdir == "" :
+        print "get_sub_script ERROR    startdir is empty."
         sys.exit()
     if outputdir == "" :
         print "get_sub_script ERROR    outputdir is empty."
@@ -81,8 +79,8 @@ def get_sub_script(ana = "", file_ = None, name = "", dataset = "", outputdir = 
     file_.write("+site_local = true\n") # run on local batch system of the site
     file_.write("+sdsc = true\n")       # run at SDSC Comet Cluster
     file_.write("+uc = true\n")         # run outside to all UC's
-    file_.write("executable = testSF.sh\n")
-    file_.write("arguments = %s %s %s\n"%(ana, dataset, outputdir))
+    file_.write("executable = CondorSFlow.sh\n")
+    file_.write("arguments = %s %s %s\n"%(ana, dataset, startdir, outputdir))
     file_.write("should_transfer_files = YES\n")
     file_.write("when_to_transfer_output = ON_EXIT\n")
     file_.write("log = %s/%s.log\n"%(log_dir, name))
