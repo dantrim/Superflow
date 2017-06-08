f0e1450bde984393e133610551ff45f62f7699c8
Daniel Antrim <dantrim@uci.edu>
2017-06-08 10:59:44 -0700

    add new geters for condor jobs

---
+++ run/get_filelist.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import sys
+
+if __name__ == "__main__" :
+    group_name = sys.argv[1]
+    process_number = sys.argv[2]
+    split_dsids = sys.argv[3]
+
+    samples_to_split = []
+    if split_dsids != "X" :
+        samples_to_split = split_dsids.split(",")
+
+    injob_filelist = "joblist_%s.txt"%group_name
+    samples = []
+
+    lines = open(injob_filelist).readlines()
+    for line in lines :
+        if not line : continue
+        line = line.strip()
+
+        split_this_sample = False
+
+        for ds in samples_to_split :
+            if ds in line :
+                split_this_sample = True
+
+        if split_this_sample :
+            rname = "./filelists/%s/%s"%(group_name, line) 
+            rfiles = open(rname).readlines()
+            for rf in rfiles :
+                if not rf : continue
+                rf = rf.strip()
+                samples.append(rf)
+        else :
+            samples.append(line)
+
+    n_total = len(samples)
+    if int(process_number) >= n_total :
+        print "get_filelist.py    ERROR input process number %d is out of range, there are only %d samples in process\n"%(int(process_number), n_total)
+    else :
+        submit_sample = samples[int(process_number)]
+        if "susyNt.root" in submit_sample :
+            print submit_sample
+        else :
+            submit_sample = "./filelists/%s/%s"%(group_name, submit_sample)
+            print submit_sample
+    
---
+++ run/get_joblog_name.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import sys
+import glob
+
+if __name__ == "__main__" :
+    
+    process_group_dir = sys.argv[1]
+    process_number = sys.argv[2]
+    split_dsids = sys.argv[3]
+
+    samples_to_split = []
+    if split_dsids != "X" :
+        samples_to_split = split_dsids.split(",")
+
+    if not process_group_dir.endswith("/") :
+        process_group_dir = process_group_dir + "/"
+    dsid_lists = glob.glob(process_group_dir + "*.txt")
+
+    container_names = []
+    all_names = []
+    containers = {}
+
+    global_process_no = 0
+    for ids, ds in enumerate(dsid_lists) :
+
+        containers[ids] = None
+
+        is_split = False
+        for sds in samples_to_split :
+            if sds in ds :
+                is_split = True
+
+        p_list = []
+        if is_split :
+            lines = open(ds).readlines()
+            for line in lines :
+                if not line : continue
+                line = line.strip()
+                all_names.append(line)
+                p_list.append(global_process_no)
+                global_process_no += 1
+
+        else :
+            all_names.append(ds)
+            p_list.append(global_process_no)
+            global_process_no += 1
+
+        containers[ids] = p_list
+
+    # find which global container this process number belongs to
+    global_to_use = -1
+    found_it = False
+    for global_id, process_id_list in containers.iteritems() :
+        print global_id
+        if found_it :
+            break
+        for p_id in process_id_list :
+            print " > ",p_id
+            #print "p_id number : %d %d"%(p_id, process_number)
+            if int(p_id) == int(process_number) :
+                global_to_use = global_id
+                found_it = True
+                break
+
+    if global_to_use < 0 :
+        print "ERROR did not find global idx"
+        sys.exit()
+    global_name = dsid_lists[global_to_use]
+    print "GLOBAL : %s"%global_name
+                
+        
