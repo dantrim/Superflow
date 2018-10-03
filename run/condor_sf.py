1f700155d1bf9ce81686b64e96f544daa9e64fd1
Daniel Antrim <dantrim@uci.edu>
2018-10-03 15:45:07 -0700

    condor_sf now handles splitting of samples

--- run/condor_sf.py
+++ run/condor_sf.py
@@ -7,7 +7,7 @@ import argparse
 import subprocess
 import time
 
-sf_exec_name = "ntupler_nn"
+sf_exec_name = "ntupler_val"
 out_dir = "/data/uclhc/uci/user/dantrim/ntuples/n0303/mc/"
 log_dir = "/data/uclhc/uci/user/dantrim/ntuples/n0303/mc/logs/"
 
@@ -15,20 +15,51 @@ filelist_dir = "/data/uclhc/uci/user/dantrim/n0303val/susynt-read/filelists/"
 
 storage_prefixes = { "mwt2" : "root://fax.mwt2.org:1094/" }
 
-samples = [ "ttbar" ]
-dsids_to_split = []# "410472" ]
+samples = [ 'ttbar_test',  'rare_top' ]
+dsids_to_split = [ "410472" ]
 
 run_with_systematics = False
 tar_file = '/data/uclhc/uci/user/dantrim/n0303val/area.tgz'
 tarred_dir = 'susynt-read'
-use_sumw_file = True
+use_sumw_file = False
 
 do_brick = True
 do_gp = True
 do_uc = True
 do_sdsc = False
 
+def special_prefixes() :
+
+    return { 'mwt2' : 'root://fax.mwt2.org:1094/',
+              'atlas-xrd' : 'root://atlas-xrd-us.usatlas.org/',
+                'env' : os.environ.get('STORAGEPREFIX','') }
+
+def sflow_exec_arg_string() :
+
+    global use_sumw_file
+    global run_with_systematics
+
+    sys_string = '-c'
+    if run_with_systematics :
+        sys_string = '-a'
+
+    sflow_args = ' %s ' % sys_string
+    if use_sumw_file :
+        sflow_args += ' --sumw ./susynt-read/sumw_file.root '
+
+    print 'WARNING ONLY LOOPING OVER 50 EVENTS'
+    sflow_args += ' -n 50 '
+
+    return sflow_args
+
 def create_prefixed_lists(base_list_dir, storage_prefix) :
+
+    if storage_prefix in special_prefixes() :
+        if special_prefixes()[storage_prefix] :
+            storage_prefix = special_prefixes()[storage_prefix]
+        else :
+            print 'ERROR Requested special prefix (=%s) is invalid' % storage_prefix
+            sys.exit()
 
     os.chdir(base_list_dir)
 
@@ -158,6 +189,8 @@ def bool_string(boolean) :
 
 def make_condor_file(sample, queue_list, condor_filename, exec_name) :
 
+    global tar_file
+
     with open(condor_filename, 'w') as f :
 
         f.write('universe = vanilla\n')
@@ -167,6 +200,7 @@ def make_condor_file(sample, queue_list, condor_filename, exec_name) :
         f.write('+sdsc=%s\n' % bool_string(do_sdsc))
         f.write('executable = %s\n' % exec_name)
         f.write('should_transfer_files = YES\n')
+        f.write('transfer_input_files = %s\n' % tar_file)
         f.write('use_x509userproxy = True\n')
         f.write('notification = Never\n')
 
@@ -174,7 +208,7 @@ def make_condor_file(sample, queue_list, condor_filename, exec_name) :
 
             injob_filelist = './' + filelist_dir[filelist_dir.find(tarred_dir):] + '/'
             injob_filelist += sample
-            injob_filelist += '/%s' % q
+            injob_filelist += '/%s' % q.split('/')[-1]
 
             log_base = q.split('/')[-1].replace('.txt','')
             sys_string = '-c'
@@ -183,9 +217,7 @@ def make_condor_file(sample, queue_list, condor_filename, exec_name) :
 
             arg_string = ' %s %s %s %s ' % (sf_exec_name, tarred_dir, injob_filelist, q)
 
-            sflow_args = ' %s ' % sys_string
-            if use_sumw_file :
-                sflow_args += ' --sumw ./susynt-read/sumw_file.root '
+            sflow_args = sflow_exec_arg_string()
 
             arg_string += sflow_args
 
@@ -208,7 +240,7 @@ def make_executable(exec_name) :
         f.write('tarred_dir=${2}\n')
         f.write('injob_filelist=${3}\n')
         f.write('sample_list=${4}\n')
-        f.write('sflow_options=${5}\n')
+        f.write('sflow_options=${@:5}\n')
         f.write('echo "   SF executable         : ${sf_exec}"\n')
         f.write('echo "   tarred directory      : ${tarred_dir}"\n')
         f.write('echo "   injob filelist loc    : ${injob_filelist}"\n')
@@ -217,24 +249,101 @@ def make_executable(exec_name) :
         f.write('while (( "$#" )); do\n')
         f.write('   shift\n')
         f.write('done\n\n')
-        f.write('startdir=${PWD}\n')
         f.write('echo "untarring area.tgz"\n')
         f.write('tar -xvf area.tgz\n\n')
         f.write('echo "current directory structure:"\n')
         f.write('ls -ltrh\n\n')
         f.write('export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase\n')
         f.write('source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh\n')
-        f.write('echo "cd ${stored_dir}"\n') 
-        f.write('cd ${stored_dir}\n')
+        f.write('echo "moving"\n') 
+        f.write('pushd ${tarred_dir}\n')
         f.write('echo "current directory structure:"\n')
         f.write('ls -ltrh\n')
         f.write('lsetup fax\n')
+        f.write('asetup AnalysisBase,21.2.45\n')
         f.write('source build/x86*/setup.sh\n')
-        f.write('echo "cd ${startdir}"\n')
+        f.write('echo "moving"\n')
+        f.write('popd\n')
+        f.write('echo "current directory structure:"\n')
+        f.write('ls -ltrh\n')
         f.write('echo "calling: ${sf_exec} -i ${injob_filelist} ${sflow_options}"\n')
         f.write('${sf_exec} -i ${injob_filelist} ${sflow_options}\n')
         f.write('echo "final directory structure:"\n')
         f.write('ls -ltrh\n')
+        f.write('echo "finish: `date`"\n')
+
+def make_condor_file_split(dsid, split_files, condor_filename, exec_name) :
+
+    global tar_file
+    with open(condor_filename, 'w') as f :
+        f.write('universe = vanilla\n')
+        f.write('+local=%s\n' % bool_string(do_brick))
+        f.write('+site_local=%s\n' % bool_string(do_gp))
+        f.write('+uc=%s\n' % bool_string(do_uc))
+        f.write('+sdsc=%s\n' % bool_string(do_sdsc))
+        f.write('executable = %s\n' % exec_name)
+        f.write('should_transfer_files = YES\n')
+        f.write('transfer_input_files = %s\n' % tar_file)
+        f.write('use_x509userproxy = True\n')
+        f.write('notification = Never\n')
+
+        for ifile, split_file in enumerate(split_files) :
+
+            arg_string = ' %d %s %s %s %s ' % (ifile, sf_exec_name, tarred_dir, split_file, dsid)
+            sflow_args = sflow_exec_arg_string()
+            arg_string += sflow_args
+
+            f.write('\n')
+            f.write('arguments = %s\n' % arg_string)
+            f.write('output = log_%s_%d.out\n' % (str(dsid), ifile))
+            f.write('log = log_%s_%d.log\n' % (str(dsid), ifile))
+            f.write('error = log_%s_%d.err\n' % (str(dsid), ifile))
+            f.write('queue\n')
+
+def make_executable_split(exec_name) :
+
+    with open(exec_name, 'w') as f :
+        f.write('#!/bin/bash\n\n\n')
+        f.write('echo "------------ %s -------------"\n' % exec_name)
+        f.write('hostname\n')
+        f.write('echo "start: `date`"\n')
+        f.write('split_idx=${1}\n')
+        f.write('sf_exec=${2}\n')
+        f.write('tarred_dir=${3}\n')
+        f.write('filename=${4}\n')
+        f.write('dsid=${5}\n')
+        f.write('sflow_options=${@:6}\n')
+        f.write('echo "    split idx        : ${split_idx}"\n')
+        f.write('echo "    SF executable    : ${sf_exec}"\n')
+        f.write('echo "    tarred directory : ${tarred_dir}"\n')
+        f.write('echo "    split file       : ${filename}"\n')
+        f.write('echo "    sample dsid      : ${dsid}"\n')
+        f.write('echo "    sflow options    : ${sflow_options}"\n')
+        f.write('while (( "$#" )); do\n')
+        f.write('   shift\n')
+        f.write('done\n')
+        f.write('echo "untarring area.tgz"\n')
+        f.write('tar -xvf area.tgz\n\n')
+        f.write('echo "current directory structure:"\n')
+        f.write('ls -ltrh\n\n')
+        f.write('export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase\n')
+        f.write('source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh\n')
+        f.write('echo "moving"\n')
+        f.write('pushd ${tarred_dir}\n')
+        f.write('echo "current directory structure:"\n')
+        f.write('ls -ltrh\n')
+        f.write('lsetup fax\n')
+        f.write('asetup AnalysisBase,21.2.45\n')
+        f.write('source build/x86*/setup.sh\n')
+        f.write('echo "moving"\n')
+        f.write('popd\n')
+        f.write('echo "current directory structure:"\n')
+        f.write('ls -ltrh\n')
+        f.write('echo "calling: ${sf_exec} -i ${injob_filelist} ${sflow_options}"\n')
+        f.write('${sf_exec} -i ${filename} ${sflow_options} --suffix ${split_idx}\n')
+        f.write('echo "final directory structure:"\n')
+        f.write('ls -ltrh\n')
+        f.write('echo "finish: `date`"\n')
 
 def submit_sample(sample, verbose = False) :
 
@@ -265,15 +374,50 @@ def submit_sample(sample, verbose = False) :
         if verbose :
             print 'Current dir: %s' % os.getcwd()
 
-        print 'making queue list'
         condor_filename = 'submit_%s.condor' % sample
         exec_name = 'run_condor_sf_%s.sh' % sample
         make_condor_file(sample, queue_list, condor_filename, exec_name)
         make_executable(exec_name)
 
+        cmd = 'condor_submit %s' % condor_filename
+        subprocess.call(cmd, shell = True)
+
         os.chdir(start_path)
         if verbose :
             print 'Current dir: %s' % os.getcwd()
+
+    elif len(split_dict.keys()) :
+
+        if verbose :
+            print 'Launching split samples'
+
+        os.chdir(start_path)
+
+        for dsid in split_dict :
+
+
+            split_dir = 'output_%s' % str(dsid)
+            cmd = 'mkdir -p %s' % split_dir
+            subprocess.call(cmd, shell = True)
+
+            full_split_dir = os.path.abspath(split_dir) 
+            os.chdir(full_split_dir)
+
+            if verbose :
+                print 'Current dir: %s' % os.getcwd()
+
+            condor_filename = 'submit_split_%s.condor' % str(dsid)
+            exec_name = 'run_condor_split_%s.sh' % str(dsid)
+            make_condor_file_split(dsid, split_dict[dsid], condor_filename, exec_name)
+            make_executable_split(exec_name)
+
+            cmd = 'condor_submit %s' % condor_filename
+            subprocess.call(cmd, shell = True)
+            os.chdir(start_path)
+            if verbose :
+                print 'Current dir: %s' % os.getcwd()
+            
+
 
 def get_samples(args) :
 
@@ -296,9 +440,16 @@ def main() :
         help = 'Set name of tar file')
     parser.add_argument('--prefix', default = 'root://fax.mwt2.org:1094/',
         help = 'Provide FAX STORAGEPREFIX to use (used when creating filelists for tar file creation')
+    parser.add_argument('--prefixes', action = 'store_true', default = False,
+        help = 'Print the list of known/common FAX prefixes')
     parser.add_argument('--skip-list-creation', default = False, action = 'store_true',
         help = 'Do not build filelists during tar file creation, use already existing \"filelist/\" dir')
     args = parser.parse_args()
+
+    if args.prefixes :
+        for key, prefix in special_prefixes().items() :
+            print ' %s : %s' % (key, prefix)
+        return
 
     if args.tar :
         create_tar(args)
@@ -309,10 +460,8 @@ def main() :
 
         n_samples = len(samples)
         for isample, sample in enumerate(samples) :
-            print '[%02d/%02d] Submitting %s' % (isample+1, n_samples, sample)
+            print '\n[%02d/%02d] Submitting %s' % (isample+1, n_samples, sample)
             submit_sample(sample, args.verbose)
-
-    
 
 #____________________________
 if __name__ == '__main__' :
