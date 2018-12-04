8b1d32b1173d7f44a980516c04336982b3760ec9
Daniel Antrim <dantrim@uci.edu>
2018-12-03 17:00:56 -0800

    normal eventweight includes btag and jvtSF

--- Root/Superweight.cxx
+++ Root/Superweight.cxx
@@ -23,14 +23,14 @@ double Superweight::product()
 {
     // dantrim July 2017 -- the user should be smart about using the product method,
     // I personally don't recommend it since the SF are analysis/selection dependent
-    #warning REDUCED EVENT WEIGHT PRODUCT
-    return susynt * lepSf; // * jvtSf; // * btagSf;
+    //#warning REDUCED EVENT WEIGHT PRODUCT
+    return susynt * lepSf * jvtSf * btagSf;
     //return susynt * pileup * lepSf * jvtSf * btagSf;
 }
 //////////////////////////////////////////////////////////////////////////////
 double Superweight::product_multi()
 {
-    return susynt_multi * lepSf;
+    return susynt_multi * lepSf * jvtSf * btagSf;
 }
 
 } // namespace
